b7481e5cdc1d3f23dcfa37d80d09a72b2e3f38b4
朱坚 <zhujian@zhujiandeMBP.mshome.net>
2017-03-16 17:46:53 +0800

    bug fix

--- app/src/main/java/com/jzhu/study/mf/base/BaseGankIoFunc.java
+++ app/src/main/java/com/jzhu/study/mf/base/BaseGankIoFunc.java
@@ -7,7 +7,7 @@ import rx.functions.Func1;
 public class BaseGankIoFunc<T> implements Func1<BaseGankIoResp<T>, Observable<T>> {
     @Override
     public Observable<T> call(BaseGankIoResp<T> resp) {
-        if (!resp.getError()) {
+        if (resp.getError()) {
             return Observable.error(new BusinessGankIoException());
         }
         return Observable.just(resp.getResults());
