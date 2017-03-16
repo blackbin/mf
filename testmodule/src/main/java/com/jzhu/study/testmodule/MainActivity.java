db6c75ab657246c6a99fbaf2bbc5c27e28041d77
朱坚 <zhujian@zhujiandeMBP.mshome.net>
2017-03-16 18:59:31 +0800

    bug fix

--- testmodule/src/main/java/com/jzhu/study/testmodule/MainActivity.java
+++ testmodule/src/main/java/com/jzhu/study/testmodule/MainActivity.java
@@ -2,20 +2,25 @@ package com.jzhu.study.testmodule;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 
 @Route(path = "/testmodule/activity")
 public class MainActivity extends Activity {
 
+    @BindView(R2.id.hello)
+    TextView hello;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (BuildConfig.DEBUG) {
-            ARouter.openLog();     // 打印日志
-            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
-        }
+        ButterKnife.bind(this);
+        ARouter.openLog();     // 打印日志
+        ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         ARouter.init(getApplication()); // 尽可能早，推荐在Application中初始化
     }
 }
