7a02779dfc629ddcb2f8c620c814c562d32ea723
zhujian <370159662@qq.com>
2017-05-05 16:40:31 +0800

    rxbinding++

--- rxbindingmodule/src/main/java/com/example/rxbindingmodule/RxbindingActivity.java
+++ rxbindingmodule/src/main/java/com/example/rxbindingmodule/RxbindingActivity.java
@@ -1,11 +1,16 @@
 package com.example.rxbindingmodule;
 
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatCheckBox;
 import android.util.Log;
+import android.view.TextureView;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.utils.TextUtils;
 import com.jakewharton.rxbinding.view.RxView;
+import com.jakewharton.rxbinding.widget.RxCompoundButton;
 import com.jakewharton.rxbinding.widget.RxTextView;
 import com.jzhu.study.baselibrary.base.BaseActivity;
 import com.jzhu.study.baselibrary.base.utils.ToastUtils;
@@ -19,6 +24,8 @@ import rx.Observer;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.functions.Func2;
+import rx.functions.Func3;
 
 /**
  * Created by jzhu on 2017/5/3.
@@ -38,6 +45,19 @@ public class RxbindingActivity extends BaseActivity {
     @BindView(R2.id.btn_long_click)
     Button longClickBtn;
 
+    @BindView(R2.id.btn_highlight)
+    Button highLightkBtn;
+
+    @BindView(R2.id.et_username)
+    EditText usernameEt;
+
+    @BindView(R2.id.et_pwd)
+    EditText pwdEt;
+
+    @BindView(R2.id.cb_protocol)
+    AppCompatCheckBox protocolCb;
+
+
     public static int SECOND = 10;
 
     private Observable<Void> timeObservable;
@@ -53,6 +73,7 @@ public class RxbindingActivity extends BaseActivity {
         initDoubleClickListener();
         initTimerListener();
         initLongClickListener();
+        initLoginListener();
     }
 
     private void initShakeListener() {
@@ -124,19 +145,39 @@ public class RxbindingActivity extends BaseActivity {
         });
     }
 
-    private void initLongClickListener(){
+    private void initLongClickListener() {
         RxView.longClicks(longClickBtn)
                 .subscribe(new Action1<Void>() {
                     @Override
                     public void call(Void aVoid) {
-                    showToast("长按");
+                        showToast("长按");
                     }
                 });
     }
+
+    private void initLoginListener() {
+        Observable<CharSequence> usernameOb = RxTextView.textChanges(usernameEt);
+        Observable<CharSequence> pwdOb = RxTextView.textChanges(pwdEt);
+        Observable<Boolean> protocolOb = RxCompoundButton.checkedChanges(protocolCb);
+        Observable.combineLatest(usernameOb, pwdOb, protocolOb, new Func3<CharSequence, CharSequence, Boolean, Boolean>() {
+            @Override
+            public Boolean call(CharSequence username, CharSequence pwd, Boolean isChecked) {
+                return !TextUtils.isEmpty(username) && !TextUtils.isEmpty(username) && isChecked;
+            }
+        }).subscribe(new Action1<Boolean>() {
+            @Override
+            public void call(Boolean aBoolean) {
+                RxView.enabled(highLightkBtn).call(aBoolean);
+            }
+        });
+
+
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(null != timeObservable){
+        if (null != timeObservable) {
             timeObservable.unsubscribeOn(AndroidSchedulers.mainThread());
         }
     }
